ea45e22e8d0b07b11a40a525e853416fb0d71ef8
Eric Harmeling <eric.harmeling@outlook.com>
2021-02-22 14:18:32 -0500

    formatted code

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 //For secure connection:
 // const fs = require('fs');
-const { Pool } = require('pg')
+const { Pool } = require("pg");
 
 // Configure the database connection.
 
 const config = {
-    user: 'max',
-    password: 'roach',
-    host: 'localhost',
-    database: 'bank',
-    port: 26257,  
-    ssl: {
-        rejectUnauthorized: false,
-    },
-    //For secure connection:
-    /*ssl: {
+  user: "max",
+  password: "roach",
+  host: "localhost",
+  database: "bank",
+  port: 26257,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  //For secure connection:
+  /*ssl: {
         ca: fs.readFileSync('/certs/ca.crt')
             .toString()
     }*/
@@ -22,107 +22,109 @@ const config = {
 
 // Create a connection pool
 
-const pool = new Pool(config)
+const pool = new Pool(config);
 
 // Wrapper for a transaction.  This automatically re-calls the operation with
 // the client as an argument as long as the database server asks for
 // the transaction to be retried.
 
 async function retryTxn(n, max, client, operation, callback) {
-    await client.query('BEGIN;');
-    while (true) {
-        n++;
-        if (n === max) {
-            throw new Error('Max retry count reached.');
-        }
-        try {
-            await operation(client, callback);
-            await client.query('COMMIT;');
-            return;
-        }
-        catch (err) {
-            if (err.code != '40001') {
-                return callback(err);
-            }
-            else {
-                console.log('Transaction failed. Retrying transaction.');
-                console.log(err.message);
-                await client.query('ROLLBACK;', () => {
-                    console.log('Rolling back transaction.');
-                });
-                await new Promise(r => setTimeout(r, 2 ** n * 1000));
-            }
-        }
+  await client.query("BEGIN;");
+  while (true) {
+    n++;
+    if (n === max) {
+      throw new Error("Max retry count reached.");
     }
+    try {
+      await operation(client, callback);
+      await client.query("COMMIT;");
+      return;
+    } catch (err) {
+      if (err.code != "40001") {
+        return callback(err);
+      } else {
+        console.log("Transaction failed. Retrying transaction.");
+        console.log(err.message);
+        await client.query("ROLLBACK;", () => {
+          console.log("Rolling back transaction.");
+        });
+        await new Promise((r) => setTimeout(r, 2 ** n * 1000));
+      }
+    }
+  }
 }
 
 // This function is called within the first transaction. It creates a table and inserts some initial values.
 
 async function initTable(client, callback) {
-    await client.query('CREATE TABLE IF NOT EXISTS accounts (id INT PRIMARY KEY, balance INT);', callback);
-    await client.query('INSERT INTO accounts (id, balance) VALUES (1, 1000), (2, 250);', callback);
-    await client.query('SELECT id, balance FROM accounts;', callback);
+  await client.query(
+    "CREATE TABLE IF NOT EXISTS accounts (id INT PRIMARY KEY, balance INT);",
+    callback
+  );
+  await client.query(
+    "INSERT INTO accounts (id, balance) VALUES (1, 1000), (2, 250);",
+    callback
+  );
+  await client.query("SELECT id, balance FROM accounts;", callback);
 }
 
 async function transferFunds(client, callback) {
-    const from = 1;
-    const to = 2;
-    const amount = 100;
-    var statement = 'SELECT balance FROM accounts WHERE id = $1 ;';
-    var values = [from];
-    await client.query(statement, values, (err, res) => {
-        if (err) {
-            return callback(err);
-        } else if (res.rows.length === 0) {
-            console.log('account not found in table');
-            return callback(err);
-        }
-        var acctBal = res.rows[0].balance;
-        if (acctBal < amount) {
-            return callback(new Error('insufficient funds'));
-        }
-    });
+  const from = 1;
+  const to = 2;
+  const amount = 100;
+  var statement = "SELECT balance FROM accounts WHERE id = $1 ;";
+  var values = [from];
+  await client.query(statement, values, (err, res) => {
+    if (err) {
+      return callback(err);
+    } else if (res.rows.length === 0) {
+      console.log("account not found in table");
+      return callback(err);
+    }
+    var acctBal = res.rows[0].balance;
+    if (acctBal < amount) {
+      return callback(new Error("insufficient funds"));
+    }
+  });
 
-    var statement = 'UPDATE accounts SET balance = balance - $1 WHERE id = $2 ;'
-    var values = [amount, from]
-    await client.query(statement, values, callback);
+  var statement = "UPDATE accounts SET balance = balance - $1 WHERE id = $2 ;";
+  var values = [amount, from];
+  await client.query(statement, values, callback);
 
-    var statement = 'UPDATE accounts SET balance = balance + $1 WHERE id = $2 ;'
-    var values = [amount, to]
-    await client.query(statement, values, callback);
+  var statement = "UPDATE accounts SET balance = balance + $1 WHERE id = $2 ;";
+  var values = [amount, to];
+  await client.query(statement, values, callback);
 
-    var statement = 'SELECT id, balance FROM accounts;'
-    await client.query(statement, callback);
-
+  var statement = "SELECT id, balance FROM accounts;";
+  await client.query(statement, callback);
 }
 
 // Run the transactions in the connection pool
 
 (async () => {
-    // Connect to database
-    const client = await pool.connect()
+  // Connect to database
+  const client = await pool.connect();
 
-    // Callback
-    function cb(err, res) {
-        if (err) throw err;
+  // Callback
+  function cb(err, res) {
+    if (err) throw err;
 
-        if (res.rows.length > 0) {
-            console.log('New account balances:');
-            res.rows.forEach((row) => {
-                console.log(row);
-            });
-        }
+    if (res.rows.length > 0) {
+      console.log("New account balances:");
+      res.rows.forEach((row) => {
+        console.log(row);
+      });
     }
+  }
 
-    // Initialize table in transaction retry wrapper
-    console.log('Initializing table...');
-    await retryTxn(0, 15, client, initTable, cb);
+  // Initialize table in transaction retry wrapper
+  console.log("Initializing table...");
+  await retryTxn(0, 15, client, initTable, cb);
 
-    // Transfer funds in transaction retry wrapper
-    console.log('Transferring funds...');
-    await retryTxn(0, 15, client, transferFunds, cb);
+  // Transfer funds in transaction retry wrapper
+  console.log("Transferring funds...");
+  await retryTxn(0, 15, client, transferFunds, cb);
 
-    // Exit program
-    process.exit();
-
-})().catch(err => console.log(err.stack))
+  // Exit program
+  process.exit();
+})().catch((err) => console.log(err.stack));
